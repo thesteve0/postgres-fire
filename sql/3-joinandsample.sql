920699f9d970f2d765cd38342b59c70217367044
thesteve0 <steve.pousty@gmail.com>
2020-08-28 17:56:10 -0700

    more progress

--- sql/3-joinandsample.sql
+++ sql/3-joinandsample.sql
@@ -4,7 +4,18 @@
 -- First table will be just the join of fires with their weather info into 1 big table that combines all th fields. This will be useful if we ever want to explore anything about the weather with fire size, shape, or other geography variables
 
 -- There are numerous days with multiple fires, I think we will need to group by alarm date and do a CTE
-select
+with grouped_fire as (
+    select alarm_date, count(*) as numfires, string_agg(fire_name, ', ') as names, st_collect(geom)::geometry(geometrycollection, 3310) as geom from ncalfire group by alarm_date
+) select w.*, grouped_fire.*, 1 as hasfire into fire_weather from weather w, grouped_fire where grouped_fire.alarm_date = w.date_time::date;
+
+-- turn the geometrycollection back into multipolygons
+alter table fire_weather alter column geom type geometry(multipolygon,3310) using st_collectionextract(geom, 3);
+
+-- Now we sample the same number of weather days that did not have fire
+with non_fire_weather as (
+    select count(weather.*)
+    from weather where id not in (select id from fire_weather)
+) TODO FINISH THIS STATMENT TO SAMPLE THE RIGHT NUMBER OF RECORDS;
 
 -- Next table is weather variables with 1 and 0s, no fire information
 
