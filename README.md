117eee4b08ddb5db8655f779a18026d1796297b4
thesteve0 <steve.pousty@gmail.com>
2020-09-11 17:59:03 -0700

    need to move machines and consoles don't transfer. Need to translate to format()

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Steps
 1. Download the data, create the db, add the postgis extension, & import the pg_dump file. FILE = 1-createload.sh
 2. Restrict the fire data only to the N. California region since our Weather data is only from the Morgan Hill Area. Put it in a new table ncal_fire FILE = 2-norcal.sql
 3. Pull all the weather corresponding to fire days. Remove 10% of the fires and put in training data and randomly sample weather days without replacement.  Total n of non-fire weather =  1.5x the number of fires. We pull this out first because we will use the withheld data to test the triggers and such
-4. TODO Center and Standardize the weather variables. Store it in the same table as new columns - with a cs_ prefix on the name
+4. TODO Center and Standardize the weather variables. Store it in the same table as new columns - with a cs_ prefix on the name. This could also be done with a view but we don't want to execute that query every time we do other analysis
 5. REACH TODO - write an update trigger that calculates updates whenever new weather data is added
 6. TODO Build the model - probably do this in R or Python and then make it into a procedure
     * Save a graph into the DB - the table for graphs should be
--- sql/4-centerstandardize.sql
+++ sql/4-centerstandardize.sql
@@ -1,2 +1,39 @@
 --  Center and Standardize the weather variables.
+https://www.postgresql.org/docs/current/plpgsql-statements.html#PLPGSQL-STATEMENTS-EXECUTING-DYN
+https://www.postgresql.org/docs/current/functions-string.html#FUNCTIONS-STRING-FORMAT
 --  Store it in the same table as new columns - with a cs_ prefix on the name
+with summary as (
+    select avg(precip), stddev(precip)
+    from final.analysis
+)
+select precip, (precip - avg)/stddev from final.analysis, summary;
+
+create or replace function final.initial_center_standardize(table_name text, prefix text, column_names text[])
+    returns text as
+$$
+DECLARE
+    col text;
+    new_col_name text;
+BEGIN
+    -- loop through column names
+    foreach col in ARRAY column_names
+        LOOP
+            -- Create the column
+            new_col_name := prefix || col;
+            -- EXECUTE starts the statement because we don't want to cache the plan since each iteration of the loop is new SQL
+            EXECUTE format('alter table %I add column %I numeric', table_name, new_col_name);
+            raise info 'After adding the column';
+
+
+            -- calculate the data
+            EXECUTE 'insert into $3 ($2) with summary as ( select avg($1), stddev($1) from $3)' ||
+                    'select  ($1 - avg)/stddev from $3, summary;'
+                USING col, new_col_name, table_name;
+        END LOOP;
+    return 'done';
+END;
+$$ LANGUAGE plpgsql;
+
+select final.initial_center_standardize('final'::text, 'test'::text, 'cs_'::text, array ['precip', 'eto', 'solar', 'rh_max']::text[]);
+
+create table final.test as select * from final.analysis;
