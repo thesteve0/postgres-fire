2a415b4787af2a84f6728cc6ad98a64a8b6b49c9
thesteve0 <steve.pousty@gmail.com>
2020-12-01 15:12:32 -0800

    getting ready for the blog post

--- sql/4-centerstandardize.sql
+++ sql/4-centerstandardize.sql
@@ -14,7 +14,7 @@ $$
 DECLARE
     col text;
     new_col_name text;
-    debug_string text;
+
 BEGIN
     -- loop through column names
     foreach col in ARRAY column_names
@@ -23,12 +23,6 @@ BEGIN
             new_col_name := prefix || col;
             -- EXECUTE starts the statement because we don't want to cache the plan since each iteration of the loop is new SQL
             EXECUTE format('alter table %I.%I add column %I numeric', schema_name, table_name, new_col_name);
-            -- raise info 'After adding the column';
-
-            debug_string := FORMAT('with summary as ( select avg(%1$I) as avg, stddev(%1$I) as stddev from %3$I.%4$I), ' ||
-                                   'final_select as (%5$I, (%1$I - avg)/stddev as centered from %3$I.%4$I cross join summary) ' ||
-                                   'update %3$I.%4$I  set %2$I = final_select.centered from final_select where final_select.%5$I =  %3$I.%4$I.%5$I ', col, new_col_name, schema_name, table_name, pkey)::text;
-            -- raise info ' Here is the string:: %', debug_string;
             -- calculate the data
             EXECUTE FORMAT('with summary as ( select avg(%1$I) as avg, stddev(%1$I) as stddev from %3$I.%4$I), ' ||
                            'final_select as (select %5$I, (%1$I - avg)/stddev as centered from %3$I.%4$I cross join summary) ' ||
